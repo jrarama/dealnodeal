1c68e28f71f0335fe5ab7d5220e776a65e4d5ea2
Joshua Rarama <joshuararama@gmail.com>
2020-02-08 02:26:31 +0800

    Updates

---
+++ precache-manifest.dcc12cd9208228d05be6dcee66bba537.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42afa5f1537fbe4b1c87bae53e3c6c39",
+    "url": "/dealnodeal/index.html"
+  },
+  {
+    "revision": "cf78fd41847b9be010f4",
+    "url": "/dealnodeal/static/css/main.177add63.chunk.css"
+  },
+  {
+    "revision": "ede1054180b187d01aaa",
+    "url": "/dealnodeal/static/js/2.97f8ad41.chunk.js"
+  },
+  {
+    "revision": "27df78d2822439c2fc110c0a385025d0",
+    "url": "/dealnodeal/static/js/2.97f8ad41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf78fd41847b9be010f4",
+    "url": "/dealnodeal/static/js/main.fe8fbfa2.chunk.js"
+  },
+  {
+    "revision": "e74d9dfcb5d7f16c9af8",
+    "url": "/dealnodeal/static/js/runtime-main.0f22c9bc.js"
+  },
+  {
+    "revision": "a3af13d3289fe649a5789a931e3e1221",
+    "url": "/dealnodeal/static/media/background.a3af13d3.jpg"
+  },
+  {
+    "revision": "76ceefa83e0422c6b19601078fe76fb8",
+    "url": "/dealnodeal/static/media/closed-case.76ceefa8.png"
+  },
+  {
+    "revision": "3d2c4d483a07612ce6e64705634ce79f",
+    "url": "/dealnodeal/static/media/happy.3d2c4d48.png"
+  },
+  {
+    "revision": "0bc4cb60087ffd0463366233d79ee2ef",
+    "url": "/dealnodeal/static/media/open-case.0bc4cb60.png"
+  },
+  {
+    "revision": "500b46929a0197fa5b4e22f1f91737af",
+    "url": "/dealnodeal/static/media/sad.500b4692.png"
+  },
+  {
+    "revision": "c31c1a617a2ec45dff3a54500f2aac9d",
+    "url": "/dealnodeal/static/media/title.c31c1a61.png"
+  }
+]);
